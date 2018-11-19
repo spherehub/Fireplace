5e2cedabfdba8168ec3894fd9958efd5ccab606a
SoraKatadzuma <dungeonsandanime@gmail.com>
2018-11-19 05:33:31 -0600

    Version mistake

--- inc/fireplace.h
+++ inc/fireplace.h
@@ -44,7 +44,7 @@
 // Versioning.
 #define FIREPLACE_MAJOR 0
 #define FIREPLACE_MINOR 0
-#define FIREPLACE_PATCH 11
+#define FIREPLACE_PATCH 13
 
 // Context rendering apis.
 #ifdef _FIREPLACE_OPENGL
