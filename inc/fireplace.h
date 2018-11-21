85490999559c15bd2043c9ef2037c291f7864b78
SoraKatadzuma <dungeonsandanime@gmail.com>
2018-11-21 07:46:35 -0600

    File name change for consistency

--- inc/fireplace.h
+++ inc/fireplace.h
@@ -44,7 +44,7 @@
 // Versioning.
 #define FIREPLACE_MAJOR 0
 #define FIREPLACE_MINOR 0
-#define FIREPLACE_PATCH 23
+#define FIREPLACE_PATCH 24
 
 // Known Fireplace errors.
 #define FAILED_INITIALIZATION   0x01001200
