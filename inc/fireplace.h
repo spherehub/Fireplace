7545fd03c1f3d5b413b14bf92e5f67720c4301a0
SoraKatadzuma <dungeonsandanime@gmail.com>
2018-11-24 07:16:14 -0600

    Forgets to inc patch

--- inc/fireplace.h
+++ inc/fireplace.h
@@ -46,7 +46,7 @@
 // Versioning.
 #define FIREPLACE_MAJOR 0
 #define FIREPLACE_MINOR 0
-#define FIREPLACE_PATCH 54
+#define FIREPLACE_PATCH 56
 
 // Context rendering apis.
 #ifdef _FIREPLACE_OPENGL
